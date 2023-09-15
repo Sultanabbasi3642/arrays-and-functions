68ff7cae8ede9287efca52b44bc4e664717e6c16
sultanabbasi3642 <ameersultanabbasi1998@gmail.com>
2023-09-15 09:08:57 -0700

    arrays

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 // let C=mycar.lastIndexOf("etron") 
+Object.defineProperty(exports, "__esModule", { value: true });
 // console.log(C);
 // let bikes :string[][]=[]
 // bikes.push(mycar.concat(cars,myFavCars))
@@ -9,10 +10,10 @@
 // console.log(myFavCars);
 // console.log(bikes)
 // console.log(`this is the reverse of cars :::${cars.reverse()}`);
-let mycar = ["bmw", "hoda", "bmw"];
-let cars = ["sonata", "ferrari", "bmw", "bmw", "hoda", "bmw",];
-let myFavCars = ["mercides", "etron"];
-let x = cars.slice().reverse();
+// let mycar :string[]= ["bmw","hoda","bmw"]
+// let cars:string[]=["sonata","ferrari","bmw","bmw","hoda","bmw",]
+// let myFavCars:string[]=["mercides","etron"]
+// let x =cars.slice().reverse()
 // console.log(cars);
 // console.log(x);
 // console.log(mycar.shift());
@@ -20,8 +21,49 @@ let x = cars.slice().reverse();
 // console.log(cars);
 // console.log(cars.pop());
 // console.log(cars);
-myFavCars.unshift(`mehran`);
-console.log(myFavCars);
-let bottle = ["coke", "fanta", "string", "ajsdhflkuh"];
-console.log(bottle.slice(0, 2));
-console.log(bottle);
+// myFavCars.unshift(`mehran`)
+// console.log(myFavCars);
+// let bottle :string[]=['coke','fanta','string','dew']
+// console.log(bottle.slice(0,2))
+// console.log(bottle);
+// bottle.splice(1,2,"big","for")
+// console.log(bottle);
+// bottle.toString()
+// console.log(bottle);
+// interface Book {
+//     title: string;
+//     author: string;
+//     year: number;
+//   }
+//   const books: Book[] = [
+//     { title: "Book 1", author: "Iqbal", year: 2010 },
+//     { title: "Book 2", author: "Faiz", year: 2015 },
+//     { title: "Book 3", author: "Iqbal", year: 2020 },
+//     { title: "Book 4", author: "Mirza", year: 2005 },
+//     { title: "Book 5", author: "Iqbal", year: 2018 },
+//   ];
+//   let Prompt = require('prompt-sync')()
+//   let authorname = Prompt("Put author name pleaseeeeeee::::")
+//   const recentBooks = books.filter((a) => a.author === authorname );
+//   console.log( `Books by ${authorname} are these `);
+//   recentBooks.forEach((b) => {
+//     console.log(b.title);
+//   })
+// const enrichedBooks = books.map((book) => ({
+//     title: book.title,
+//     author: book.author,
+//     year: book.year,
+//     isRecent: book.year > 2010,
+//   }));
+//   console.log(enrichedBooks);
+// let numbers:number[]=[1,2,3,4,5];
+// let reduce=numbers.reduceRight((a,b)=> a*b)
+// console.log(reduce);
+// let Biscuits:string[]=["Tuck","super","Rio","candy","tiger"];
+// let Findmybiscuit=Biscuits.map((mybiscuits)=> mybiscuits == "Tiger")
+// console.log(Findmybiscuit);
+// let sortedbiscuits = Biscuits.sort()
+// console.log(sortedbiscuits);
+// console.log(Biscuits.fill("Zeera",1,4))
+// Biscuits.splice(1,3,"fdd","good","ddd");
+// console.log(Biscuits);
--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 
 // let C=mycar.lastIndexOf("etron") 
 
+import { log } from "console";
+
 // console.log(C);
 
 // let bikes :string[][]=[]
@@ -15,10 +17,10 @@
 // console.log(bikes)
 // console.log(`this is the reverse of cars :::${cars.reverse()}`);
 
-let mycar :string[]= ["bmw","hoda","bmw"]
-let cars:string[]=["sonata","ferrari","bmw","bmw","hoda","bmw",]
-let myFavCars:string[]=["mercides","etron"]
-let x =cars.slice().reverse()
+// let mycar :string[]= ["bmw","hoda","bmw"]
+// let cars:string[]=["sonata","ferrari","bmw","bmw","hoda","bmw",]
+// let myFavCars:string[]=["mercides","etron"]
+// let x =cars.slice().reverse()
 
 // console.log(cars);
 // console.log(x);
@@ -34,11 +36,76 @@ let x =cars.slice().reverse()
 // console.log(cars);
 
 
-myFavCars.unshift(`mehran`)
-console.log(myFavCars);
+// myFavCars.unshift(`mehran`)
+// console.log(myFavCars);
 
-let bottle :string[]=["coke","fanta","string","ajsdhflkuh"]
+// let bottle :string[]=['coke','fanta','string','dew']
 
-console.log(bottle.slice(0,2))
+// console.log(bottle.slice(0,2))
 
-console.log(bottle);
+// console.log(bottle);
+
+
+// bottle.splice(1,2,"big","for")
+// console.log(bottle);
+
+// bottle.toString()
+
+// console.log(bottle);
+
+
+
+// interface Book {
+//     title: string;
+//     author: string;
+//     year: number;
+//   }
+  
+//   const books: Book[] = [
+//     { title: "Book 1", author: "Iqbal", year: 2010 },
+//     { title: "Book 2", author: "Faiz", year: 2015 },
+//     { title: "Book 3", author: "Iqbal", year: 2020 },
+//     { title: "Book 4", author: "Mirza", year: 2005 },
+//     { title: "Book 5", author: "Iqbal", year: 2018 },
+//   ];
+  
+//   let Prompt = require('prompt-sync')()
+//   let authorname = Prompt("Put author name pleaseeeeeee::::")
+
+//   const recentBooks = books.filter((a) => a.author === authorname );
+  
+
+//   console.log( `Books by ${authorname} are these `);
+//   recentBooks.forEach((b) => {
+//     console.log(b.title);
+//   })
+
+
+// const enrichedBooks = books.map((book) => ({
+//     title: book.title,
+//     author: book.author,
+//     year: book.year,
+//     isRecent: book.year > 2010,
+//   }));
+  
+//   console.log(enrichedBooks);
+
+
+// let numbers:number[]=[1,2,3,4,5];
+// let reduce=numbers.reduceRight((a,b)=> a*b)
+// console.log(reduce);
+
+
+// let Biscuits:string[]=["Tuck","super","Rio","candy","tiger"];
+// let Findmybiscuit=Biscuits.map((mybiscuits)=> mybiscuits == "Tiger")
+// console.log(Findmybiscuit);
+// let sortedbiscuits = Biscuits.sort()
+// console.log(sortedbiscuits);
+
+
+// console.log(Biscuits.fill("Zeera",1,4))
+
+// Biscuits.splice(1,3,"fdd","good","ddd");
+// console.log(Biscuits);
+
+
